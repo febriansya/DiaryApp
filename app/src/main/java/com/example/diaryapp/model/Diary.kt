58a96e76fa3976ff9b0907badd1e5277df0296be
yan <darmansyahyan21@gmail.com>
2023-04-08 22:22:00 +0800

    Mod

---
+++ app/src/main/java/com/example/diaryapp/model/Diary.kt
@@ -0,0 +1,18 @@
+package com.example.diaryapp.model
+
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.ObjectId
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+
+class Diary : RealmObject {
+    @PrimaryKey
+    var id: ObjectId = ObjectId.create()
+    val ownerId: String = ""
+    var title: String = ""
+    var description: String = ""
+    var image: RealmList<String> = realmListOf()
+    var data: RealmInstant = RealmInstant.from(System.currentTimeMillis(), 0)
+}
--- app/src/main/java/com/example/diaryapp/presentation/components/AlertDIalog.kt
+++ app/src/main/java/com/example/diaryapp/presentation/components/AlertDIalog.kt
@@ -3,7 +3,6 @@ package com.example.diaryapp.presentation.components
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Device
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 
