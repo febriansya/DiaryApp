e96b7fb53ec00aecc135db234866c48657dde5ea
yan <darmansyahyan21@gmail.com>
2023-08-16 20:41:45 +0800

    create diary holder component

--- app/src/main/java/com/bulleh/diaryapp/presentation/components/DiaryHolder.kt
+++ app/src/main/java/com/bulleh/diaryapp/presentation/components/DiaryHolder.kt
@@ -1,10 +1,13 @@
 package com.bulleh.diaryapp.presentation.components
 
+import android.graphics.fonts.FontStyle
+import android.icu.util.LocaleData
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,13 +26,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bulleh.diaryapp.model.Diary
 import com.bulleh.diaryapp.model.Mood
 import com.bulleh.diaryapp.ui.theme.Elevation
+import com.bulleh.diaryapp.util.toInstant
 import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
@@ -36,29 +45,58 @@ import java.util.Locale
 
 @Composable
 fun DiaryHolder(
-    diary: Diary,
-    onClick: (String) -> Unit
+    diary: Diary, onClick: (String) -> Unit
 ) {
+
+    /*
+    * local densitiy to mark height  as current
+    * */
+    val localDensity = LocalDensity.current
+
     var componentHeight by remember {
         mutableStateOf(0.dp)
     }
 
-    Row(modifier = Modifier
-        .clickable(
-            indication = null,
-            interactionSource = remember {
-                MutableInteractionSource()
-            }
-        ) { onClick(diary._id.toString()) }) {
+    Row(modifier = Modifier.clickable(indication = null, interactionSource = remember {
+        MutableInteractionSource()
+    }) { onClick(diary._id.toString()) }) {
 
         Spacer(modifier = Modifier.width(14.dp))
         Surface(
             modifier = Modifier
-                .width(14.dp)
-                .height(14.dp),
+                .width(2.dp)
+                .height(componentHeight + 14.dp),
             tonalElevation = Elevation.Level1
         ) {}
         Spacer(modifier = Modifier.width(20.dp))
+        Surface(
+            modifier = Modifier
+                .clip(shape = Shapes().medium)
+
+                /*
+                * make height surface based on local and update the componentHeight
+                * */
+                .onGloballyPositioned {
+                    componentHeight = with(localDensity) {
+                        it.size.height.toDp()
+                    }
+                },
+
+            tonalElevation = Elevation.Level1
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                DiaryHeader(moodName = diary.mood, time = diary.date.toInstant())
+                Text(
+                    modifier = Modifier.padding(all = 14.dp),
+                    text = diary.description,
+                    style = TextStyle(fontSize = MaterialTheme.typography.bodyLarge.fontSize),
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
     }
 }
 
@@ -74,9 +112,8 @@ fun DiaryHeader(moodName: String, time: Instant) {
         mutableStateOf(Mood.valueOf(moodName))
     }
     val formatter = remember {
-        DateTimeFormatter.ofPattern("hh:mm:a", Locale.getDefault()).withZone(
-            ZoneId.systemDefault()
-        )
+        DateTimeFormatter.ofPattern("hh:mm a", Locale.getDefault())
+            .withZone(ZoneId.systemDefault())
     }
 
     Row(
@@ -105,8 +142,19 @@ fun DiaryHeader(moodName: String, time: Instant) {
 
         Text(
             text = formatter.format(time),
-            color = mood.containerColor,
+            color = mood.contentColor,
             style = TextStyle(fontSize = MaterialTheme.typography.bodyMedium.fontSize)
         )
     }
+}
+
+
+@Preview
+@Composable
+fun DiaryHolderPreviewItem() {
+    DiaryHolder(diary = Diary().apply {
+        title = "Dear Recruiter"
+        description = "lorem ipsum bla bla aefaeaef aefaefaefvarbbav arafaeflaekfajef aefpajefohae"
+        mood = Mood.Happy.name
+    }, onClick = {})
 }
